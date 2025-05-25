17f7c6569d8a8af5e7a56831e282eaae93b159b7
meersen <>
2025-05-25 21:32:37 +0200

    changed audio configuration to use pipewire instead of pulseaudio

--- os/audio.nix
+++ os/audio.nix
@@ -1,3 +1,8 @@
-{
-  hardware.pulseaudio.enable = true;
+{ pkgs, lib, ... }: {
+  hardware.pulseaudio.enable = lib.mkForce false;
+
+  security.rtkit.enable = true;
+  services.pipewire.enable = true;
+  services.pipewire.alsa.enable = true;
+  services.pipewire.pulse.enable = true;
 }
