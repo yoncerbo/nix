a686e7ecc2475902c0dbe1c1643a85c29edb18f0
meersen <>
2025-06-10 04:51:02 +0200

    Disabled firewall

--- os/networking.nix
+++ os/networking.nix
@@ -3,6 +3,6 @@
     networkmanager.enable = true;
     useDHCP = lib.mkDefault true;
     # firewall.allowedTCPPorts = [ 8080 80 443 ];
-    firewall.enable = true;
+    firewall.enable = false;
   };
 }
