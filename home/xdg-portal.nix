b6defdc9bba54394c20a66eef5d6672479f0a79d
meersen <>
2025-06-10 04:56:18 +0200

    Updated xdg-portal config to include screencasting

--- home/xdg-portal.nix
+++ home/xdg-portal.nix
@@ -2,7 +2,13 @@
   xdg.portal = {
     enable = true;
     xdgOpenUsePortal = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-gnome
+      pkgs.xdg-desktop-portal-wlr
+    ];
     config.common.default = [ "gtk" ];
+    config.common."org.freedesktop.impl.portal.Screencast" = [ "river" ];
+    config.common."org.freedesktop.portal.Screencast" = [ "river" ];
   };
 }
